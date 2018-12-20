b257c16121776a2e602b307d653294df7ce672a2
angelloparr <angellop123@gmail.com>
2018-12-20 12:52:55 -0800

    add get_rec function

--- collaborative_filtering/recommendations.py
+++ collaborative_filtering/recommendations.py
@@ -26,7 +26,7 @@ critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
 # Returns the best matches for person from the prefs dictionary.
 # Number of results and similarity functional are optional params,
 # where n is number of results and similarity is the scoring algorithm.
-def top_matches(prefs, person, n = 5, similarity = sim_pearson):
+def top_matches(prefs, person, n = 6, similarity = sim_pearson):
     scores = [(similarity(prefs, person, other), other)
                         for other in prefs if other != person]
 
@@ -35,4 +35,41 @@ def top_matches(prefs, person, n = 5, similarity = sim_pearson):
     scores.reverse()
     return scores[0:n]
 
-print(top_matches(critics, 'Toby', n = 6))
+print(top_matches(critics, 'Toby'))
+
+# Gets recommendations for a person by using a weighted average
+# of every other user's rankings.
+def get_recommendations(prefs, person, n = 6, similarity = sim_pearson):
+    totals = {}
+    similar_sums = {}
+    for other in prefs:
+        # don't compare to 'person'
+        if other != person:
+            sim = similarity(prefs, person, other)
+
+            # ignore scores < 0
+            if sim > 0:
+                for item in prefs[other]:
+
+                    # only score movies not yet seen
+                    if item not in prefs[person] or prefs[person][item] == 0:
+                        # similarity * score
+                        # item is the movie title
+                        totals.setdefault(item, 0)
+                        # each movie score for critic * similarity number
+                        totals[item] += sim * prefs[other][item]
+
+                        # sum of similarities
+                        similar_sums.setdefault(item, 0)
+                        similar_sums[item] += sim
+
+            # Create the normalized list
+            rankings = [(total/similar_sums[item], item) for item, total in totals.items()]
+
+    # Return the sorted list
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+print()
+print(get_recommendations(critics, 'Toby'))
